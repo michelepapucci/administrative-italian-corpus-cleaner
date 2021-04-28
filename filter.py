2c424fdbbe8599c4cb299727a64c2d43838ffbeb
Michele Papucci <michele.papucci97@gmail.com>
2021-04-28 19:50:54 +0200

    first-try-plotting2

--- filter.py
+++ filter.py
@@ -332,7 +332,7 @@ def filter_social(folder):
 
 if __name__ == "__main__":
     # filter_social(Path("input/social_annotati"))
-    filter_sem_web(Path("input/demo/web-56"))
+    filter_sem_web(Path("input/sem_web"))
 
 # sem = pagine web siti comuni: ok
 # pawac: rianalizzare con stanza o cercare di sfruttare la già presente analisi?
